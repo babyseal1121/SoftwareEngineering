a23fded12bb76fbff1da97d3a24817a170e2ceb8
Maaqii <103295356+Maaqii@users.noreply.github.com>
2022-12-23 08:55:29 +0800

    添加了列表

--- Studyx_Front/src/utils/navData.js
+++ Studyx_Front/src/utils/navData.js
@@ -32,5 +32,114 @@ export default[
             },
         ]
     },
-
+    {
+        name: '实验项目',
+        icon: 'el-icon-s-management',
+        path: '/experiment',
+        child: [
+            {
+                name: '实验项目管理',
+                icon: 'el-icon-document',
+                path: '/gradeset',
+                component:"UserList",
+                
+            },
+            {
+                name: '提交实验报告',
+                icon: 'el-icon-document-add',
+                path: '/gradeset',
+                component:"UserList",
+                
+            },
+            {
+                name: '批改实验报告',
+                icon: 'el-icon-document-checked',
+                path: '/gradeset',
+                component:"UserList",
+                
+            },
+            {
+                name: '我的实验报告',
+                icon: 'el-icon-folder-opened',
+                path: '/gradeset',
+                component:"UserList",
+                
+            },
+        ]
+    },
+    {
+        name: '快速测验',
+        icon: 'el-icon-tickets',
+        path: '/exercise',
+        child: [
+            {
+                name: '题目管理',
+                icon: 'el-icon-paperclip',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '发布测验',
+                icon: 'el-icon-plus',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '我的测验',
+                icon: 'el-icon-edit',
+                path: '/userlist',
+                component:"UserList",
+            },
+        ]
+    },
+    {
+        name: '实验班级',
+        icon: 'el-icon-s-order',
+        path: '/experimentClass',
+        child: [
+            {
+                name: '我的班级',
+                icon: 'el-icon-s-flag',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '实验管理',
+                icon: 'el-icon-folder',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '人员管理',
+                icon: 'el-icon-s-custom',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '班级管理',
+                icon: 'el-icon-collection-tag',
+                path: '/userlist',
+                component:"UserList",
+            },
+        ]
+    },
+    {
+        name: '学习资料',
+        icon: 'el-icon-notebook-2',
+        path: '/file',
+        child: [
+            {
+                name: '资料列表',
+                icon: 'el-icon-files',
+                path: '/userlist',
+                component:"UserList",
+            },
+            {
+                name: '资料管理',
+                icon: 'el-icon-folder-checked',
+                path: '/userlist',
+                component:"UserList",
+            },
+        ]
+    },
 ]
